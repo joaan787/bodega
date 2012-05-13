bef1204b13df99faec8b96890594e34a6e185705
Joakim Andersson <joakim.c.andersson@gmail.com>
2012-05-13 23:41:20 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bodega::Application.config.secret_token = 'd87a87d94b5107057a1b549556a393ce2a7f2963db9e9c05b8bad77a26e4b8f8ce015cbe1ed6e3bec333ed5e2ed080502821c8f337257cecb7ec84130d6d81d9'
